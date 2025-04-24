21730025d0bd1ad831485360571fc72177163673
thxwat <tanawatmiracle@gmail.com>
2025-04-24 21:02:21 +0700

    update app.js and app.test.js2

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ const monthOfTheYear = (date = new Date()) => {
 
 try {
     document.getElementById('day').innerText = dayOfTheWeek();
+    document.getElementById('month').innerText = monthOfTheYear();
 } catch(err) {}
 
 exports.dayOfTheWeek = dayOfTheWeek;
